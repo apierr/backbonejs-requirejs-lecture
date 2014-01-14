bfe7b8704a7ac8b083062bb55544d9f5919f8df8
Antonio Pierro <antonio.pierro@gmail.com>
2014-01-14 10:26:22 +0100

    first commi

---
+++ examples/final-project/src/js/app.js
@@ -0,0 +1,12 @@
+/* global */
+(function () {
+	'use strict';
+
+	define([
+	], function () {
+		
+		var ContactManager = {};
+
+		return ContactManager;
+	});
+})();
---
+++ examples/final-project/src/js/main.js
@@ -0,0 +1,13 @@
+/* global require*/
+(function () {
+	'use strict';
+
+	require([
+		'app'
+	], function (contactManager) {
+
+		console.log(contactManager);
+		
+	});
+
+})();
