bb204454f6a4bc20f2242dddc4c4f54eeece4f3f
Antonio Pierro <antonio.pierro@gmail.com>
2014-01-16 13:32:20 +0100

    added showModal to the itemUserView

--- examples/final-project/src/js/views/itemUserView.js
+++ examples/final-project/src/js/views/itemUserView.js
@@ -3,15 +3,39 @@
 	'use strict';
 
 	define([
+		'underscore',
 		'marionette',
 		'text!../../templates/itemUser.tpl'
-	], function (Marionette, itemUserTemplate) {
+	], function (_, Marionette, itemUserTemplate) {
 
 		return Marionette.ItemView.extend({
+
+			initialize: function () {
+				_.bindAll(this, 'showModal');
+			},
 
 			tagName: 'tr',
 
 			template: itemUserTemplate,
+
+			events: {
+				'click .js-edit': 'showModal'
+			},
+
+			showModal: function () {
+
+				var model = this.model;
+
+				/* TODO app should be removed from here*/
+				require([
+					'views/editView',
+					'app'
+				], function (EditView, app) {
+					app.vent.trigger('showModal', new EditView({
+						model: model
+					}));
+				});
+			}
 
 		});
 	});
