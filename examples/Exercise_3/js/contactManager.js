bf9a35f40a917373f1341b8c86d9cb07c4cf052c
Antonio Pierro <antonio.pierro@gmail.com>
2014-01-21 13:08:33 +0100

    changed the backboneView in MarionetteItemView

--- examples/Exercise_3/js/contactManager.js
+++ examples/Exercise_3/js/contactManager.js
@@ -12,70 +12,10 @@ $(function () {
 
 	});
 
-	app.Users = Backbone.Collection.extend({
+	app.Users = Backbone.Collection;
 
-		localStorage: new Backbone.LocalStorage('contact-manager') 
-
-	});
-
-	app.UserView = Backbone.View.extend({
-
-		tagName: 'li',
-
-		initialize: function () {
-			_.bindAll(this, 'render');
-		},
-
-		render: function () {
-			var template = Mustache.to_html('name: {{ firstName }}', this.model.toJSON())
-			return this.$el.html(template);
-		}
-
-	});
-
-	app.UsersView = Backbone.View.extend({
-
-		el: '#contact-manager',
-
-		template: _.template("<h2>There are <%= usersLength %> users<h2>"),
-
-		initialize: function () {
-			this.render();
-		},
-
-		render: function () {
-			this.$el.html(this.template({ usersLength: app.users.length }));
-		}
-
-	});
-
-	app.UsersViewMustache = Backbone.View.extend({
-
-		el: '#contact-manager',
-
-		initialize: function () {
-			_.bindAll(this, 'addOne', 'render');
-			this.template = Mustache.to_html("<h2>There are {{usersLength}} users<h2> <ul id='user-list'></ul>", { usersLength: app.users.length });
-			this.render();
-		},
-
-		render: function () {
-			this.$el.html(this.template);
-			this.addAll();
-		},
-
-		addAll: function () {
-			app.users.forEach(this.addOne);
-		},
-
-		addOne: function (userModel) {
-			var view = new app.UserView({
-				model: userModel
-			});
-			this.$('ul').append(view.render());
-		}
-
-
+	app.UsersViewMarionette = Marionette.ItemView.extend({
+		template: '#some-template'
 	});
 
 	app.users = new app.Users([
@@ -85,7 +25,10 @@ $(function () {
 		{firstName: 'ceonel', lastName: 'nessi', number: '123457'}
 	]);
 
-	// new  app.UsersView();
-	new  app.UsersViewMustache();
+	var view = new app.UsersViewMarionette({
+		collection: app.users
+	}).render();
+
+	$('ul').append(view.$el);
 
 });
